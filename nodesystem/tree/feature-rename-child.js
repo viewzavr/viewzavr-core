d9641d0d417c65a0bb0abdbac691898bbfc8737c
Pavel Vasev <pavel.vasev@gmail.com>
2021-02-24 16:58:13 +0500

    much better

---
+++ nodesystem/tree/feature-rename-child.js
@@ -0,0 +1,34 @@
+// фича переименовывания добавляемого объекта в списке объектов родителя
+export default function setup( obj, nf ) {
+
+  var _obj = obj;
+  obj = nf( obj );
+
+  var gcounter = 0;
+  
+  var orig = obj.appendChild;
+
+  obj.appendChild = function( cobj,name,enableRename ) {
+    if (name) {
+      // todo: check name here, and maybe rename?
+      var existing = obj.getChildByName( name );
+      if (existing && existing != cobj) {
+        if (enableRename) {
+          while (obj.getChildByName( name )) {
+            gcounter=gcounter+1;
+            name = name + "_" + gcounter.toString();
+          }
+        }
+        else {
+          console.error( "WARNING: overwriting existing child with same name and removing existing child!",name );
+          // obj.forgetChild( existing );
+          existing.remove();
+        }
+      }
+    }
+    return orig( cobj, name, enableRename );
+  }
+    
+
+  return obj;
+}
